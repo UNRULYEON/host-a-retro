dfd4afb5afefa3baff997d9ade3312f2700891bf
Amar Kisoensingh <amar@kisoensingh.sh>
2023-09-20 00:36:35 +0200

    Add instructions

--- src/steps/Intro.tsx
+++ src/steps/Intro.tsx
@@ -1,4 +1,13 @@
+import { Button } from "@/components/ui/button";
 import { useRetroState } from "@/state";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 
 const IntroStep = () => {
   const introGif = useRetroState((state) => state.introGif);
@@ -7,7 +16,36 @@ const IntroStep = () => {
   const handleRandomiseIntroGif = () => randomiseIntroGif();
 
   return (
-    <div className="flex h-[inherit] w-[inherit] items-center justify-center">
+    <div className="relative flex h-[inherit] w-[inherit] flex-col items-center justify-center gap-8">
+      <Dialog>
+        <DialogTrigger asChild>
+          <Button className="absolute top-0">How it works</Button>
+        </DialogTrigger>
+        <DialogContent>
+          <DialogHeader className="text-left">
+            <DialogTitle className="mb-4 text-4xl">Easy as 1-2-3</DialogTitle>
+            <DialogDescription className="flex flex-col gap-2 text-lg">
+              <div className="flex flex-row gap-2">
+                <span>🎯</span>
+                <span>
+                  Select at the top at what step you're at in your retro
+                </span>
+              </div>
+              <div className="flex flex-row gap-2">
+                <span>⏱️</span>
+                <span>Use the timer to timebox</span>
+              </div>
+              <div className="flex flex-row gap-2">
+                <span>🎲</span>
+                <span>
+                  Energiser and retro themes are randomised. Reroll them if
+                  you're feeling lucky
+                </span>
+              </div>
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
       <img
         className="max-h-min w-full cursor-pointer rounded-lg md:w-[70%]"
         src={introGif}
