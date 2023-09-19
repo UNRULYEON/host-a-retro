ea71cecfefd60051c36c59b351fb22256b9ea141
Amar Kisoensingh <amar@kisoensingh.sh>
2023-09-20 00:23:40 +0200

    Rewrite energisers and retro themes

--- src/components/markdownComponents.tsx
+++ src/components/markdownComponents.tsx
@@ -2,6 +2,7 @@ import { MDXComponents } from "mdx/types";
 
 const components: MDXComponents = {
   h1: (props) => <h1 className="mb-4 text-4xl font-bold" {...props} />,
+  h2: (props) => <h2 className="mb-4 text-3xl font-bold" {...props} />,
   p: (props) => <p className="mb-4 text-lg" {...props} />,
   ol: (props) => <ol className="mb-2 ml-5 list-decimal text-lg" {...props} />,
   ul: (props) => <ul className="mb-2 ml-5 list-disc text-lg" {...props} />,
