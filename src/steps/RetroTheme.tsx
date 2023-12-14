a0bd644e922e0268dccf33ea4a4467f7087d3cff
Amar Kisoensingh <amar@kisoensingh.sh>
2023-12-14 08:38:52 +0100

    Make retro theme actions sticky

--- src/steps/RetroTheme.tsx
+++ src/steps/RetroTheme.tsx
@@ -19,15 +19,15 @@ const RetroThemeStep = () => {
   return (
     <Card
       ref={ref}
-      className="m-auto flex h-[inherit] w-[inherit] max-w-2xl flex-col justify-between gap-4 overflow-x-auto rounded-lg p-4"
+      className="m-auto flex h-[inherit] w-[inherit] max-w-2xl flex-col justify-between gap-4 overflow-x-auto rounded-lg"
     >
-      <div>
+      <div className="p-4">
         {retroTheme.emoji && (
           <span className="mb-4 flex text-4xl">{retroTheme.emoji}</span>
         )}
         <retroTheme.default />
       </div>
-      <div className="flex flex-row items-center justify-between gap-4">
+      <div className="sticky bottom-0 flex flex-row items-center justify-between border-t bg-background p-2">
         <a href={retroTheme.source} target="_blank">
           <Badge variant="secondary">{retroTheme.source}</Badge>
         </a>
